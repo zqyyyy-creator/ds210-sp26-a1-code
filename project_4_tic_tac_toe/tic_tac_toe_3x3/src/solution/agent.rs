5add1ecaea63193e254b72d6090c5d8269dcf72c
zqyyyy <zqyyyy@bu.edu>
2026-04-11 15:19:51 -0400

    Finish for recursive structure

--- project_4_tic_tac_toe/tic_tac_toe_3x3/src/solution/agent.rs
+++ project_4_tic_tac_toe/tic_tac_toe_3x3/src/solution/agent.rs
@@ -11,8 +11,25 @@ impl Agent for SolutionAgent {
     // where <score> is your estimate for the score of the game
     // and <x>, <y> are the position of the move your solution will make.
     fn solve(board: &mut Board, player: Player, _time_limit: u64) -> (i32, usize, usize) {
+        if board.game_over() {
+            return (board.score(), 0, 0);
+        };
+        let moves = board.moves();
+        let mut best_move = moves[0];
+        let mut best_score = match player {
+            Player::X => i32::MIN,
+            Player::O => i32::MAX,
+        };
+        let next_player = player.flip();
+        for m in moves {
+            board.apply_move(m, player);
+            let (score, _, _) = SolutionAgent::solve(board, next_player, _time_limit);
+            board.undo_move(m, player);
+            // Add recursive call here
+
+        }
         // If you want to make a recursive call to this solution, use
         // `SolutionAgent::solve(...)`
-        unimplemented!("Not yet implemented")
+        return (best_score, best_move.0, best_move.1);
     }
 }
